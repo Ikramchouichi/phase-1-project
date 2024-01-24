b3d4ac0239eaf0f3b84075b05c190717ca9f6dba
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-01-24 12:08:07 -0500

    commit to merge

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
         .then(cities => {
           cities.forEach(renderCity);
         });
-<<<<<<< HEAD
     }
 
     function renderCity(city) {
@@ -56,53 +55,3 @@
 
       commentsList.appendChild(newCommentItem);
     }
-=======
-  }
-  
-  
-  
-  
-  function renderCities(city) {
-    const cardsContainer = document.querySelector('.landing-page-cards-container');
-    
-    const cardContainer = document.createElement('div');
-    cardContainer.className = 'place-card-container';
-
-    const cardContent = document.createElement('div');
-    cardContent.className = 'place-card-container1';
-
-    const img = document.createElement('img');
-    img.src = city.image;
-    img.className = 'place-card-image';
-    cardContent.appendChild(img);
-
-    const span = document.createElement('span');
-    span.textContent = city.name;
-    cardContent.appendChild(span);
-
-    const descriptionSpan = document.createElement('span');
-    descriptionSpan.textContent = city.description;
-    cardContent.appendChild(descriptionSpan);
-
-
-    cardContainer.appendChild(cardContent);
-    cardsContainer.appendChild(cardContainer);
-}
-function changeBackgroundImage() {
-    const bannerContainer = document.querySelector('.landing-page-top-container');
-
-    bannerContainer.addEventListener('mouseover', function() {
-        this.style.backgroundImage = "url('./images/travel1.png')"; // New image when mouseover
-    });
-
-    bannerContainer.addEventListener('mouseout', function() {
-        this.style.backgroundImage = "url('./images/travel.png')"; // Original image when mouseout
-    });
-}
-
-// Call the functions
-document.addEventListener('DOMContentLoaded', () => {
-    getAllCities();
-    changeBackgroundImage();
-});
->>>>>>> 88bf1641fc8e539388da29d4f29ecab82d1fffe3
